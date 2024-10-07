a5c92f60990425839281e23ad8d61489bf92a7dc
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-10-06 23:08:45 -0400

    Finish jpg exif stripping

--- ExifStripper/Program.cs
+++ ExifStripper/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace ExifStripper;
+﻿using ExifLibrary;
+
+namespace ExifStripper;
 
 internal static class Program
 {
     internal static void Main(string[] args)
     {
-        
+        var files = Directory.GetFiles(args[0]);
+        var targetPath = Path.Join(args[0], "Stripped");
+        Directory.CreateDirectory(targetPath);
+        foreach (var file in files.Where(f => 
+                     string.Compare(Path.GetExtension(f), ".jpg", StringComparison.OrdinalIgnoreCase) == 0))
+        {
+            var img = ImageFile.FromFile(file);
+            for (var i = 0; i < img.Properties.Count; i++)
+            {
+                var property = img.Properties[0];
+                img.Properties.Remove(property);
+            }
+            img.Save(Path.Join(Path.GetDirectoryName(file), "Stripped", Path.GetFileName(file)));
+        }
     }
 }
